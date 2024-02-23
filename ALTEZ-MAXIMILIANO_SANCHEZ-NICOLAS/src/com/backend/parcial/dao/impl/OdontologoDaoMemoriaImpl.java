2d99821b250a68fa0ebfe23bd2b18f4b011199c1
nicos91189 <nicolassanchez91189@gmail.com>
2024-02-22 22:03:57 -0300

    se implementa OdontologoDaoMemoria

---
+++ ALTEZ-MAXIMILIANO_SANCHEZ-NICOLAS/src/com/backend/parcial/dao/impl/OdontologoDaoMemoriaImpl.java
@@ -0,0 +1,32 @@
+package com.backend.parcial.dao.impl;
+
+import com.backend.parcial.dao.IDao;
+import com.backend.parcial.entity.Odontologo;
+import org.apache.log4j.Logger;
+
+import java.util.List;
+
+public class OdontologoDaoMemoriaImpl implements IDao<Odontologo> {
+
+    private final Logger LOG = Logger.getLogger(OdontologoDaoMemoriaImpl.class);
+
+    private List<Odontologo> listaOdontologos;
+
+    public OdontologoDaoMemoriaImpl(List<Odontologo> listaOdontologos){
+        this.listaOdontologos = listaOdontologos;
+    }
+
+    @Override
+    public Odontologo guardar(Odontologo odontologo) {
+        Odontologo odontologoGuardado = new Odontologo(listaOdontologos.size()+1, odontologo.getNumeroDeMatricula(), odontologo.getNombre(), odontologo.getApellido());
+        listaOdontologos.add(odontologo);
+        LOG.info("Odontologo guardado con exito" + odontologoGuardado);
+        return odontologoGuardado;
+    }
+
+    @Override
+    public List<Odontologo> buscarTodos() {
+
+        return listaOdontologos;
+    }
+}
